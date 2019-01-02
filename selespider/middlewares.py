69bae9361244c4b6e05de98df9802e271075b3ab
ovin <31349439+ovinzhou@users.noreply.github.com>
2019-01-02 16:54:13 +0800

    create project

---
+++ selespider/middlewares.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import random
+from selenium import webdriver
+import requests
+from scrapy import signals
+from scrapy.http import HtmlResponse
+
+from selespider.settings import USER_AGENT_LIST
+
+
+class SeleniumMiddleware(object):
+    pass
+
+
+# class SelespiderSpiderMiddleware(object):
+#     # Not all methods need to be defined. If a method is not defined,
+#     # scrapy acts as if the spider middleware does not modify the
+#     # passed objects.
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         # This method is used by Scrapy to create your spiders.
+#         s = cls()
+#         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+#         return s
+#
+#     def process_spider_input(self, response, spider):
+#         # Called for each response that goes through the spider
+#         # middleware and into the spider.
+#
+#         # Should return None or raise an exception.
+#         return None
+#
+#     def process_spider_output(self, response, result, spider):
+#         # Called with the results returned from the Spider, after
+#         # it has processed the response.
+#
+#         # Must return an iterable of Request, dict or Item objects.
+#         for i in result:
+#             yield i
+#
+#     def process_spider_exception(self, response, exception, spider):
+#         # Called when a spider or process_spider_input() method
+#         # (from other spider middleware) raises an exception.
+#
+#         # Should return either None or an iterable of Response, dict
+#         # or Item objects.
+#         pass
+#
+#     def process_start_requests(self, start_requests, spider):
+#         # Called with the start requests of the spider, and works
+#         # similarly to the process_spider_output() method, except
+#         # that it doesn’t have a response associated.
+#
+#         # Must return only requests (not items).
+#         for r in start_requests:
+#             yield r
+#
+#     def spider_opened(self, spider):
+#         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class SelespiderDownloaderMiddleware(object):
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # request.url  为当前请求的URL
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        driver.get(request.url)
+        content = driver.page_source
+        driver.quit()
+        return HtmlResponse(url=request.url, body=content, request=request, status=200, encoding='utf-8')
+
+    def process_response(self, request, response, spider):
+        return response
+
+    def process_exception(self, request, exception, spider):
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class RandomUserAgentMiddleware(object):
+    def process_request(self, request, spider):
+        ua = random.choice(USER_AGENT_LIST)
+        if ua:
+            request.headers.setdefault('User-Agent', ua)
---
+++ selespider/spiders/amazon.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider, Request
+from scrapy_splash import SplashRequest
+
+
+# splash lua script
+script = """
+         function main(splash, args)
+             assert(splash:go(args.url))
+             return splash:html()
+         end
+         """
+
+
+get_url = 'https://www.amazon.cn/s/ref=nb_sb_noss_2?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99' \
+          '&url=search-alias%3Daps&field-keywords=%E6%89%8B%E6%9C%BA%E5%A3%B3'
+
+
+class AmazonSpider(Spider):
+    name = 'amazon'
+    allowed_domains = ['amazon.cn']
+
+    def start_requests(self):
+        yield SplashRequest(get_url, callback=self.parse, endpoint='execute', args={'lua_source': script})
+
+    def parse(self, response):
+        # 获取请求头
+        headers = response.request.headers.get('User-Agent')
+        print(headers.decode('utf-8'))
+        print(response)
+
+
+
